857bdaad873d738cb93bb7bf91cd26638d403dcc
jeyasuriya28 <105341224+jeyasuriya28@users.noreply.github.com>
2022-10-19 09:53:04 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,21 @@
+/*
+console is an object in js. value put inside into the parentheses will get printed. 
+console is an Object
+log() is an method or action that execute value inside parentheses.
+It can print or execute string, no, boolean, and data type values
+*/
+
+console.log(3);
+
+console.log('joy'); //string
+console.log(2); // no
+
+let isGreater = 4 > 1;
+console.log( isGreater); // boolean
+
+/*
+
+/* - used to indicate multiple line comment
+// - used ti indicate single line commet
+
+*/
