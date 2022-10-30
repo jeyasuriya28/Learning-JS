0ddfe215213e5433505f4a4b7b282e370229e77a
jeyasuriya28 <105341224+jeyasuriya28@users.noreply.github.com>
2022-10-30 20:27:50 +0530

    Add files via upload

---
+++ blockscope.js
@@ -0,0 +1,15 @@
+//blocks : it is a code that is found inside the parenthesis {}
+//block of code is a function
+//function body is actullay block of code
+
+const city = 'Newyork'
+const citySkyline = () => {
+    let skyscrapper = 'Empire state Building';
+    return 'The stars over the' + ' ' + skyscrapper + ' in ' + city;
+
+}
+
+console.log(citySkyline());
+
+
+//globalscope: variables that are declared outside the blocks.thses are called global variables. 
